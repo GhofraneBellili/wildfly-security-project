dd1ce9852c80339578b5f8ddfe4d139194d81cbf
FarahBna <farah.ibenabdallah@supcom.tn>
2026-01-14 21:55:46 +0100

    fichier.war

--- backend/src/main/java/com/evenster/EvensterApplication.java
+++ backend/src/main/java/com/evenster/EvensterApplication.java
@@ -2,9 +2,17 @@ package com.evenster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EvensterApplication {
+public class EvensterApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EvensterApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EvensterApplication.class, args);
     }
